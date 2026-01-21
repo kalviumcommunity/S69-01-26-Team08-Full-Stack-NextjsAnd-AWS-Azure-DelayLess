767e62acf036ef8650c3135b013abe784452e450
DIYA <diya94816@gmail.com>
2026-01-21 13:54:21 +0530

    mongodb

---
+++ delayless/lib/mongodb.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error("MONGODB_URI not found");
+}
+
+export async function connectDB() {
+  if (mongoose.connection.readyState >= 1) {
+    return;
+  }
+  return mongoose.connect(MONGODB_URI);
+}
